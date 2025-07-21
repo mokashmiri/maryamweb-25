e064bdc248e35cad0fe8c11550e911c096958cc8
Mo Kashmiri <119856194+mokashmiri@users.noreply.github.com>
2025-07-22 00:57:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/maryamweb-25/', // 👈 add this line
+  base: '/',           // ← add this line
   plugins: [react()],
 })
